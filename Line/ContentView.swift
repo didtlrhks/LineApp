1cf8eebc289916c7b03bb109d02c478d6a59ffb7
sikwan <yysskk99999@naver.com>
2024-03-09 21:02:28 +0900

    init

---
+++ Line/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Line
+//
+//  Created by 양시관 on 3/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Line/LineApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineApp.swift
+//  Line
+//
+//  Created by 양시관 on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct LineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
