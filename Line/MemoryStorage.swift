83c86a257c548922f2850bc2dd123fc2365976e8
sikwan <yysskk99999@naver.com>
2024-03-10 02:32:23 +0900

    PhotoPickerService

---
+++ Line/MemoryStorage.swift
@@ -0,0 +1,29 @@
+//
+//  MemoryStorage.swift
+//  Line
+//
+//  Created by 양시관 on 3/10/24.
+//
+
+import Foundation
+
+
+import UIKit
+
+protocol MemoryStorageType {
+    func value(for key: String) -> UIImage?
+    func store(for key: String, image: UIImage)
+}
+
+class MemoryStorage: MemoryStorageType {
+    
+    var cache = NSCache<NSString, UIImage>()
+    
+    func value(for key: String) -> UIImage? {
+        cache.object(forKey: NSString(string: key))
+    }
+    
+    func store(for key: String, image: UIImage) {
+        cache.setObject(image, forKey: NSString(string: key))
+    }
+}
